b9fcad5723de6858b421d6c2f8ea3b2b1e351830
HrisikeshSinghacHowdhury <hrisikesh-singha.chowdhury@quantuminventions.com>
2021-07-27 08:58:55 +0530

    after review

--- Assignment2/Assignment2_QNo2/SolAssignmentNo2_QNo2.rb
+++ Assignment2/Assignment2_QNo2/SolAssignmentNo2_QNo2.rb
@@ -1,45 +1,55 @@
 # => 2. Write a class (with constructor) to define instance
 # => methods for getting area & perimeter of a rectangle.
 
-# => Buisness logic class
-class Rectangle
-  def initialize(length, breadth)
-    @length = length
-    @breadth = breadth
+# => Business logic class
+class RectangleSkeleton
+
+  attr_accessor :len, :br
+
+  # => defining fn to initialize instance variables
+  def initialize(len, br)
+
+    self.len = len
+    self.br = br
   end
+
   # => return area of a rectangle
   def get_area
-    @length * @breadth
+    len * br
   end
+
   # => return perimeter of a rectangle
   def get_perimeter
-    2 * (@length + @breadth)
+    2 * (len + br)
   end
+
 end
 
 # => Define user interface class
-class CallRectangle
-  @@cnt = 1
-  def CallRectangle.call_rectangle_ops
+class RectangleInterfaceSkeleton
+
+  def self.call_rectangle_ops
+
     res = "y"
     while (res == "y") do
-        # => Create object as required of the above Rectangle class
-        puts "Creating object.of Rect #{@@cnt}..."
-        puts "--------------"
-        puts "Enter the length of:"
-        len = gets.chomp.to_i
-        puts "Enter the breadth of:"
-        br = gets.chomp.to_i
-        ob_rect = Rectangle.new(len,br)
-        puts "Area is = #{ob_rect.get_area}"
-        puts "Perimeter is =#{ob_rect.get_perimeter}"
-        puts "--------------"
-        @@cnt += 1
-        puts "Press y/n to continue/exit"
-        res = gets.chomp
+      # => Create object as required of the above Rectangle class
+      puts "--------------"
+      puts "Enter the length of:"
+      len = gets.chomp.to_i
+      puts "Enter the breadth of:"
+      br = gets.chomp.to_i
+      ob_rect = RectangleSkeleton.new(len, br)
+      puts "Area is = #{ ob_rect.get_area}"
+      puts "Perimeter is =#{ob_rect.get_perimeter}"
+      puts "--------------"
+      puts "Press y/n to continue/exit"
+      res = gets.chomp
     end
+
   end
 end
 
 # => Call using Class Methods
-CallRectangle.call_rectangle_ops
+RectangleInterfaceSkeleton.call_rectangle_ops
+
+
--- Assignment2/Assignment2_QNo3/SolAssignmentNo2_QNo3.rb
+++ Assignment2/Assignment2_QNo3/SolAssignmentNo2_QNo3.rb
@@ -1,9 +1,11 @@
 # => 3 Write an example of how you can use super method with and without arguments.
 # => Super Parent Class
 class SuperParentSkeleton
+
   def initialize
     puts "Hii You chose option : Super without Arguments in Parent Class"
   end
+
   def super_argument(num)
     puts "hii You chose option : Super with Arguments in Parent Class"
   end
@@ -11,18 +13,23 @@ end
 
 # => Super child class
 class SuperChildSkeleton < SuperParentSkeleton
+
   def initialize
+
     # super without arguments
     super
     puts "Hii You chose option : Super without Arguments in Child Class"
   end
+
   def super_argument(num)
+
     # super with arguments
     super(num)
     puts "Hii You chose option : Super with Arguments in Child Class"
-  end
+    end
 end
 
 # => Call the above SuperChildSkeleton class
 ob = SuperChildSkeleton.new
 ob.super_argument(9)
+
